a745815e69ba9f3589dbd0c948445782ad6e005c
hugozerken <lizhidong@lizhidongdeMac.local>
2014-10-16 00:19:15 +0800

    Initial Commit

---
+++ Discover/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Discover
+//
+//  Created by hugozerken on 14-10-16.
+//  Copyright (c) 2014年 com.ucweb.lixm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
